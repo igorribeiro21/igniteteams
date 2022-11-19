986c3e513ebcef1badfbdb68aa2af6de7bb011b1
igor.ribeiro <igorapalvesribeiro@gmail.com>
2022-11-19 10:45:53 -0300

    cssInJs - utlizando styled-components, definindo o tema da aplicação, adicionando fontes e criando componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
+
+import theme from './src/theme';
+
 import { Groups } from '@screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   );
 }
