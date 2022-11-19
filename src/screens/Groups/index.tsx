fb358437171fa31ea0ec23e40102d9b360debb20
igor.ribeiro <igorapalvesribeiro@gmail.com>
2022-11-19 15:00:53 -0300

    cssInJs - adicionando biblioteca de icones, criando componentes Highlight e GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,23 @@
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 import {
-  Container,
-  
+  Container
 } from './styles';
 
 export function Groups() {
   return (
     <Container>
-      <Header />    
+      <Header />
+      <Highlight
+        title='Turmas'
+        subTitle='Jogue com a sua turma'
+      />
+
+      <GroupCard
+        title='Galera do ignite'
+        onPress={() => { }}
+      />
     </Container>
   );
 }
