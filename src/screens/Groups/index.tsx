1aa5e60764a738e47fb9bec44fabe4c04604ea3d
igor.ribeiro <igorapalvesribeiro@gmail.com>
2022-11-23 21:56:25 -0300

    cssInJs - listando grupos e criando componente ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 import {
   Container
 } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
   return (
     <Container>
       <Header />
@@ -14,10 +18,23 @@ export function Groups() {
         subTitle='Jogue com a sua turma'
       />
 
-      <GroupCard
-        title='Galera do ignite'
-        onPress={() => { }}
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+            onPress={() => { }}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message='Que tal cadastrar a primeira turma?'
+          />
+        )}
       />
+
     </Container>
   );
 }
