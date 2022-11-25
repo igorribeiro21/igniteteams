6f7f91fe018d1525a1d2b3db61a87a256b7ff065
igor.ribeiro <igorapalvesribeiro@gmail.com>
2022-11-24 21:56:37 -0300

    cssInJs - criando componente Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 import {
   Container
 } from './styles';
@@ -33,6 +34,10 @@ export function Groups() {
             message='Que tal cadastrar a primeira turma?'
           />
         )}
+      />
+
+      <Button
+        title='Criar Nova Turma'
       />
 
     </Container>
