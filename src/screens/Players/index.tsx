9aa376cc8ff63a637e95af9e58ba7f911d997285
igor.ribeiro <igorapalvesribeiro@gmail.com>
2023-01-14 16:20:34 -0300

    cssInJs - feat: criando Componente ButtonIcon,deixando-o dinâmico e componente Filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,15 +1,57 @@
+
+import { FlatList } from 'react-native';
+import { useState } from 'react';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
-import { Container } from './styles';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
+
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
+
 
 export function Players() {
+    const [team, setTeam] = useState('Time A');
+    const [players, setPlayers] = useState([]);
+
     return (
         <Container>
             <Header showBackButton />
-            <Highlight 
+            <Highlight
                 title='Nome da turma'
                 subTitle='adicione a galera e separe os times'
             />
+
+            <Form>
+                <Input
+                    placeholder='Nome da pessoa'
+                    autoCorrect={false}
+                />
+                <ButtonIcon
+                    icon="add"
+                />
+            </Form>
+
+            <HeaderList>
+                <FlatList
+                    data={['Time A', 'Time B']}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
+                />
+
+                <NumberOfPlayers>
+                    {players.length}
+                </NumberOfPlayers>
+
+            </HeaderList>
 
         </Container>
     )
