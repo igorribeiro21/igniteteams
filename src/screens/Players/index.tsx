9e262ae621a8488b40d0f5809f1bb50702e9901b
igor.ribeiro <igorapalvesribeiro@gmail.com>
2023-01-15 15:38:00 -0300

    cssInJs - feat: finishing the player list

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -8,13 +8,15 @@ import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
 import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 
 
 export function Players() {
     const [team, setTeam] = useState('Time A');
-    const [players, setPlayers] = useState(['Igor','Gustavo']);
+    const [players, setPlayers] = useState([]);
 
     return (
         <Container>
@@ -60,9 +62,24 @@ export function Players() {
                 renderItem={({ item }) => (
                     <PlayerCard
                         name={item}
-                        onRemove={() => {}}
+                        onRemove={() => { }}
                     />
                 )}
+                ListEmptyComponent={() => (
+                    <ListEmpty
+                        message='Não há pessoas nesse time'
+                    />
+                )}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={[
+                    { paddingBottom: 100 },
+                    players.length === 0 && { flex: 1 }
+                ]}
+            />
+
+            <Button
+                title='Remover Turma'
+                type='SECONDARY'
             />
 
         </Container>
